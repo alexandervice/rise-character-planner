c30993493018286d473dc79f644a33c53c1130ce
Alexander Vice <alexdvice@gmail.com>
2023-06-09 11:07:30 -0700

    Update CharacterList.jsx

--- client/src/components/CharacterList.jsx
+++ client/src/components/CharacterList.jsx
@@ -33,7 +33,7 @@ const CharacterList = (props) => {
         }
         </tbody>
       </table>
-      <Link className='mr-3' to={"/characters/create"}><button className='mt-5 bg-yellow-100 hover:bg-yellow-200 rounded px-1 border-solid border-2 border-yellow-400 mb-5'>Create a New Character</button></Link>
+      <Link className='mr-3' to={"/characters/create"}><button className='mt-5 bg-yellow-200 hover:bg-yellow-300 rounded px-1 border-solid border-2 border-yellow-400 mb-5 dark:text-black dark:hover:bg-yellow-200 dark:bg-yellow-300 dark:border-yellow-100'>Create a New Character</button></Link>
     </div>
   )
 }
