982d2fc88b2e759a533a9b9f67847a0f021cc10a
Alexander Vice <alexdvice@gmail.com>
2023-06-17 15:28:04 -0700

    prepping for docker

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000; // Allow the port to be set dynamically by the hosting environment
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
+const origin = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
 require('dotenv').config();
 require("./config/mongoose.config");
 
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin}));
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
